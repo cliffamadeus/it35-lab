a4f7aac873fc43aa6faeb28cca09763de94b19a5
cliffamadeus <dev.clfm5t12@gmail.com>
2025-03-24 13:50:49 +0800

    Updates

--- assets/ios.transition-legacy-BGBnpTs2.js
+++ assets/ios.transition-legacy-BGBnpTs2.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-EE58aiHH.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-BsiQ_hSu.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-3G5BAU24.js
+++ assets/md.transition-legacy-3G5BAU24.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-EE58aiHH.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-BsiQ_hSu.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
