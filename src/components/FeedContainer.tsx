8d375f8366acf65891dbccc386bcd6c89e359c53
cliffamadeus <dev.clfm5t12@gmail.com>
2025-04-02 15:28:44 +0800

    Create post ui changes

--- src/components/FeedContainer.tsx
+++ src/components/FeedContainer.tsx
@@ -117,15 +117,17 @@ const FeedContainer = () => {
         <IonContent>
           {user ? (
             <>
-              <IonCard>
+            <IonCard>
                 <IonCardHeader>
-                  <IonCardTitle>Create Post</IonCardTitle>
+                    <IonCardTitle>Create Post</IonCardTitle>
                 </IonCardHeader>
                 <IonCardContent>
-                  <IonInput value={postContent} onIonChange={e => setPostContent(e.detail.value!)} placeholder="Write a post..." />
-                  <IonButton onClick={createPost}>Post</IonButton>
+                    <IonInput value={postContent} onIonChange={e => setPostContent(e.detail.value!)} placeholder="Write a post..." />
                 </IonCardContent>
-              </IonCard>
+                <div style={{ display: 'flex', justifyContent: 'flex-end', padding: '0.5rem' }}>
+                    <IonButton onClick={createPost}>Post</IonButton>
+                </div>
+            </IonCard>
 
               {posts.map(post => (
                 <IonCard key={post.post_id} style={{ marginTop: '2rem' }}>
