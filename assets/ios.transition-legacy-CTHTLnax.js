3f1724483574bcbb77bbeeea957c3052ab0b85bf
cliffamadeus <dev.clfm5t12@gmail.com>
2025-04-21 08:42:16 +0800

    Updates

--- assets/ios.transition-legacy-CTHTLnax.js
+++ assets/ios.transition-legacy-CTHTLnax.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DOh2V53C.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-B7h-Bkg0.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
